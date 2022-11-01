ca082e81c5ee831c4f3b8ab95c6a39d2c218c502
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2022-11-01 20:25:07 +0700

    Update info-gcbot.js

--- plugins/info-gcbot.js
+++ plugins/info-gcbot.js
@@ -3,13 +3,13 @@ import fetch from 'node-fetch'
  let handler = async(m, { conn }) => { 
 
          let pp = fs.readFileSync('./thumbnail.jpg')
-         let gc1 = 'https://chat.whatsapp.com/JkdB3gSCwCRGgOdCRfkAt1'
-         let caption = `*Mʏ Gᴄ Oғғɪᴄɪᴀʟ*`
+         let gc1 = 'https://chat.whatsapp.com/GeWPLmclHaVHsF0GymCcJz'
+         let caption = `*MASUK GC BOT LAH BANH*`
          await conn.sendButtonDoc(m.chat, caption, wm, 'Owner', '.owner', m, { contextInfo: { 
              externalAdReply: {
                sourceUrl: `${gc1}`,
-               title: 'My Group Official', 
-               body: 'victor Md',
+               title: 'My Group Official', 
+               body: '® san.id',
                thumbnail: pp
              } 
          }})
