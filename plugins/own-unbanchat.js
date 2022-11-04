a067a13e04ffa76882de2b5746e8b2b5d1323061
Kasanstore7 <114809701+Kasanstore7@users.noreply.github.com>
2022-11-04 13:36:51 +0700

    Update own-unbanchat.js

--- plugins/own-unbanchat.js
+++ plugins/own-unbanchat.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
-    global.db.data.chats[m.chat].isBanned = false
-    m.reply('Done!')
+    global.db.data.chats[m.chat].isBanned = true
+    m.reply('Done Makanya jan main" ama owner:v!')
 }
 handler.help = ['unbanchat']
 handler.tags = ['owner']
